009cfd5909772e40107eac29126accef211ae0c6
ycleo <leoyclee@gmail.com>
2024-01-21 22:24:11 -0800

    added apply form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,18 +29,6 @@ import { HousingService } from '../housing.service';
 })
 
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-
-  // housingLocation: HousingLocation = {
-  //   id: 9999,
-  //   name: 'Test Home',
-  //   city: 'Test city',
-  //   state: 'ST',
-  //   photo: `${this.baseUrl}/example-house.jpg`,
-  //   availableUnits: 99,
-  //   wifi: true,
-  //   laundry: false,
-  // };
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
   constructor() {
